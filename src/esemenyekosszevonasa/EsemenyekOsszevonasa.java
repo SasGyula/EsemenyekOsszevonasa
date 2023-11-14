d28c82b1517a85707af8e3323632a0bfab971ea8
SasGyula <gyulasass43@gmail.com>
2023-11-14 12:18:26 +0100

    konstruktor használata

--- src/esemenyekosszevonasa/EsemenyekOsszevonasa.java
+++ src/esemenyekosszevonasa/EsemenyekOsszevonasa.java
@@ -17,6 +17,8 @@ public class EsemenyekOsszevonasa extends javax.swing.JFrame {
      */
     public EsemenyekOsszevonasa() {
         initComponents();
+        osszeg = 0;
+        jTextField1.setText(osszeg + " ft");
     }
 
     /**
